192734558b7edd99086fd078109bc5cab0c6eb10
Edison <aalevenegass@gmail.com>
2023-11-14 16:57:38 -0300

    Agregando comentarios en código javaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,25 @@
+// variables que guardan un elemento del DOM
 let inputNuevaTarea = document.getElementById("input");
 let btnNuevaTarea = document.getElementById("btn");
 let idNuevaTarea = document.getElementById("id");
 let contentNuevaTarea = document.getElementById("tareas");
 let totalTareas = document.getElementById("total");
+let tareasRealizadas = document.getElementById("realizadas");
 
+// arreglo de objetos con tareas por defecto
 let arrayList = [
   { id: 16, tarea: "Hacer el aseo", realizada: false },
   { id: 17, tarea: "Limpiar la cocina", realizada: false },
   { id: 18, tarea: "Hacer mercado", realizada: false },
 ];
 
+//funcion que renderiza las tareas agregadas por defecto
 function renderArrayList() {
   let htmlListTarea = "";
   let htmlListId = "";
   for (const item of arrayList) {
-    htmlListTarea += `<p class="span__text">${item.tarea}<input class="checkbox" type="checkbox" name="" id="check"><button class="btn-2" onclick="borrar(${item.id})">Eliminar</button></p>`;
+    htmlListTarea += `<p class="span__text">${item.tarea}<input class="checkbox" type="checkbox" name="" id="check">
+    <button class="btn-2" onclick="borrar(${item.id})">Eliminar</button></p>`;
     htmlListId += ` <p class="span__text">${item.id}</p>`;
   }
   contentNuevaTarea.innerHTML = htmlListTarea;
@@ -23,13 +28,15 @@ function renderArrayList() {
 }
 renderArrayList();
 
-function borrar(id) {
+// función que borra un elemento agregado al arreglo por su id
+function borrar() {
   let indice = arrayList.findIndex((list) => list.id == id);
   arrayList.splice(indice, 1);
 
   renderArrayList();
 }
 
+// función que agrega una nueva tarea al ingresarla en el input
 btnNuevaTarea.addEventListener("click", () => {
   let inputValue = inputNuevaTarea.value;
   if (inputNuevaTarea.value === "") {

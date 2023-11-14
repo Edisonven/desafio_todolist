613350e3d380c8111131c535ce73e5fd62aac1d1
Edison <aalevenegass@gmail.com>
2023-11-14 14:55:35 -0300

    Modificando id de objetos añadidos por defecto

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,16 @@ let contentNuevaTarea = document.getElementById("tareas");
 let totalTareas = document.getElementById("total");
 
 let arrayList = [
-  { id: Date.now(), tarea: "Hacer el aseo" },
-  { id: Date.now(), tarea: "Limpiar la cocina" },
-  { id: Date.now(), tarea: "Hacer mercado" },
+  { id: 16, tarea: "Hacer el aseo", realizada: false },
+  { id: 17, tarea: "Limpiar la cocina", realizada: false },
+  { id: 18, tarea: "Hacer mercado", realizada: false },
 ];
 
 function renderArrayList() {
   let htmlListTarea = "";
   let htmlListId = "";
   for (const item of arrayList) {
-    htmlListTarea += `<p class="span__text">${item.tarea}<input class="checkbox" type="checkbox" name="" id=""><button class="btn-2" onclick="borrar(${item.id})">Eliminar</button></p>`;
+    htmlListTarea += `<p class="span__text">${item.tarea}<input class="checkbox" type="checkbox" name="" id="check"><button class="btn-2" onclick="borrar(${item.id})">Eliminar</button></p>`;
     htmlListId += ` <p class="span__text">${item.id}</p>`;
   }
   contentNuevaTarea.innerHTML = htmlListTarea;
@@ -24,8 +24,9 @@ function renderArrayList() {
 renderArrayList();
 
 function borrar(id) {
-  let indice = arrayList.findIndex((ele) => ele.id == id);
+  let indice = arrayList.findIndex((list) => list.id == id);
   arrayList.splice(indice, 1);
 
   renderArrayList();
 }
+
